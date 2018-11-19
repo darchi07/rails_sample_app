48bc00b974a8472ad501dc80a11afbc56d07bb73
darchi07 <kokuryousei@gmail.com>
2018-11-19 18:15:58 +0900

    ユーザをDBから見つけてから検証する

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:password])
+    if user && user.authenticate(params[:session][:password])
+      #ユーザログイン後にユーザ情報のページにリダイレクトする
+    else
+      #エラーメッセージを作成する
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+
 end
