80d8c0b0284b7909d4c5b0687e4f4951dff8f3ac
darchi07 <kokuryousei@gmail.com>
2018-11-28 21:24:36 +0900

    ログイン成功時の条件文のタイプミスを修正

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,11 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:password])
+    user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       #ユーザログイン後にユーザ情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       #エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
