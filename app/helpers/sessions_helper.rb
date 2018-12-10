8c1b635e6b5435385459612dbf3933677f35b4f2
darchi07 <kokuryousei@gmail.com>
2018-12-10 20:01:44 +0900

    ユーザを記憶する

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,13 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+  
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
 
   # 現在ログイン中のユーザを返す（いる場合）
   def current_user
